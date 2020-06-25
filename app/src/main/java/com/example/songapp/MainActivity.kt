f6d072e6f8885ecc71d7909c13edbaad70b19cd4
Solis <imsolis@hotmail.com>
2020-06-25 12:49:29 -0500

    Práctica con Kotlin

---
+++ app/src/main/java/com/example/songapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.songapp
+
+import android.app.AlertDialog
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialogo.view.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMain.setOnClickListener(){
+            saluda()
+        }
+    }
+
+    fun saluda(){
+        val nombre = findViewById<EditText>(R.id.nombre);
+        val nuevosaludo:String =nombre.getText().toString();
+        val dialog = LayoutInflater.from(this).inflate(R.layout.dialogo, null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(dialog)
+            .setTitle("Bienvenido $nuevosaludo")
+        val  mAlertDialog = mBuilder.show()
+        dialog.cerrar.setOnClickListener(){
+            mAlertDialog.dismiss()
+        }
+    }
+}
